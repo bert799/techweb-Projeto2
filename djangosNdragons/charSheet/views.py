1bb78e6f000bd6853385044c5d1cfb3755b295ff
bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2021-10-26 08:22:13 -0300

    projeto configurado

---
+++ djangosNdragons/charSheet/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+#from .models import Note, Tag
+#from .serializers import NoteSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import Http404
+import urllib
+
+
+def index(request):
+    return
--- djangosNdragons/djangosNdragons/settings.py
+++ djangosNdragons/djangosNdragons/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'charSheet.apps.CharsheetConfig',
+    'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
